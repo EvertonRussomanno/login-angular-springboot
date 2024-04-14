23542892f7d8822dce8084466cd5fe36742dc49a
Everton Martins <evertonmartins154@gmail.com>
2024-04-14 10:05:42 -0300

    Backend dtos, controllers and cors

---
+++ backend/login-auth-api/src/main/java/com/martinseverton/loginauthapi/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.martinseverton.loginauthapi.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ backend/login-auth-api/src/main/java/com/martinseverton/loginauthapi/dto/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.martinseverton.loginauthapi.dto;
+
+public record UserResponseDTO(String name, String token) {
+}
